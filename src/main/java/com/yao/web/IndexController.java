a62957f4bab6400b70a097ac77c1bc8e4f6d7230
Chin-Ho-Yao <q82717182q@gmail.com>
2021-08-23 22:19:30 +0800

    #38全局搜尋like、form表單指定方法與位置、onclick搜尋鈕觸發submit、搜索個數、

--- src/main/java/com/yao/web/IndexController.java
+++ src/main/java/com/yao/web/IndexController.java
@@ -3,6 +3,7 @@ package com.yao.web;
 import com.yao.service.BlogService;
 import com.yao.service.TagService;
 import com.yao.service.TypeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -10,9 +11,14 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 /*web控制器*/
+@Slf4j
 @Controller
 public class IndexController {
+
 
     @Autowired
     private BlogService blogService;
@@ -25,18 +31,32 @@ public class IndexController {
 
     /*找路徑，製作分頁@PageableDefault*/
     @GetMapping("/")
-    public String index(@PageableDefault(size = 8,sort = {"updateTime"},direction = Sort.Direction.DESC) Pageable pageable, Model model) {
-
-        model.addAttribute("page",blogService.listBlog(pageable));
-        model.addAttribute("types",typeService.listTypeTop(6));/*這可以寫在配置文件靈活的運用，指定為6條數據，這邊先寫死*/
+    public String index(@PageableDefault(size = 8, sort = {"updateTime"}, direction = Sort.Direction.DESC) Pageable pageable, Model model) {
+        model.addAttribute("page", blogService.listBlog(pageable));
+        model.addAttribute("types", typeService.listTypeTop(6));/*這可以寫在配置文件靈活的運用，指定為6條數據，這邊先寫死*/
         /*index的tag處理*/
-        model.addAttribute("tags",tagService.listTagTop(10));/*這可以寫在配置文件靈活的運用，指定為10條數據，這邊先寫死*/
+        model.addAttribute("tags", tagService.listTagTop(10));/*這可以寫在配置文件靈活的運用，指定為10條數據，這邊先寫死*/
         /*#37首頁最新推薦表格數據獲取*/
-        model.addAttribute("recommendBlogs",blogService.listRecommendBlogTop(8));/*指定為8條數據，這邊先寫死*/
+        model.addAttribute("recommendBlogs", blogService.listRecommendBlogTop(8));/*指定為8條數據，這邊先寫死*/
 
         System.out.println("------index------");
         return "index";/*返回index.html*/
     }
+
+    @PostMapping("/search")
+    public String Search(@PageableDefault(size = 8, sort = {"updateTime"},
+            direction = Sort.Direction.DESC) Pageable pageable,
+                         @RequestParam String query, Model model) {
+        if (query == null) {
+            log.warn("query is null.");
+        }
+        model.addAttribute("page", blogService.listBlog("%"+query+"%", pageable));
+        model.addAttribute("query", query);
+
+        return "search";
+    }
+
+
     @GetMapping("/blog/{id}")
     public String blog() {
         System.out.println("------index------");
