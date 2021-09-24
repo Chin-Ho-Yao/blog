399eac2570420520b53e122121145a31db8b5386
Chin-Ho-Yao <q82717182q@gmail.com>
2021-09-24 23:37:48 +0800

    SpringBootBlog#44博客評論功能-4 TypeShowController、type選中顏色、解析渲染後端、導航鏈結、、、、、

---
+++ src/main/java/com/yao/web/TypeShowController.java
@@ -0,0 +1,54 @@
+package com.yao.web;
+
+import com.yao.po.Type;
+import com.yao.service.BlogService;
+import com.yao.service.TypeService;
+import com.yao.vo.BlogQuery;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+/**
+ * Created by Jack Yao on 2021/9/22 10:06 下午
+ */
+@Slf4j
+@Controller
+public class TypeShowController {
+
+    @Autowired
+    private TypeService typeService;
+
+    @Autowired
+    private BlogService blogService;
+
+    @GetMapping("/types/{id}")
+    public String type(@PageableDefault(size = 8, sort = {"updateTime"}, direction = Sort.Direction.DESC) Pageable pageable,
+                       @PathVariable Long id, Model model){
+
+        List<Type> types = typeService.listTypeTop(10000);
+
+        if (id == -1){
+            id = types.get(0).getId();
+        }
+        BlogQuery blogQuery = new BlogQuery();
+        blogQuery.setTypeId(id);
+        model.addAttribute("types",types);
+        model.addAttribute("page",blogService.listBlog(pageable,blogQuery));
+        model.addAttribute("activeTypeId",id);
+        log.info(" - XXXXX - model - OOOOO - : " + model);
+
+
+        log.info(" - XXXXX - return types - OOOOO - : ");
+        return "/types";
+    }
+
+
+}
